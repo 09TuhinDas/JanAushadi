25c557d44aeb6de955234b28f849f30d759e9cbb
Tuhin Das <tuhin@Tuhins-MacBook-Air.local>
2024-06-14 23:19:28 +0530

    minor changes

--- src/Frontend/components1/Movingitms.jsx
+++ src/Frontend/components1/Movingitms.jsx
@@ -5,33 +5,35 @@ import { NavLink } from "react-router-dom";
 function Movingitms() {
   return (
     <div className="Movingitms">
-      <div className="mt-[80px] ml-[15px] w-[560px] h-[270px] text-center bg-white rounded-2xl border border-black border-solid">
-        <div className=" gap-5 px-12 py-3  rounded-2xl border-0 border-black border-solid shadow-sm bg-zinc-300 bg-opacity-10  ">
-          <div className="flex gap-5 justify-between text-3xl font-bold text-black">
-            <button className="justify-center px-4 py-4 border border-solid hover:bg-fuchsia-600 border-black border-opacity-50 rounded-[30.859px] active:border-white duration-300 active:text-white">
-              <li className="list-none" href="/">
-                Slow Moving
-              </li>
-            </button>
-            <button className="justify-center px-6 py-5 bg-white border border-solid hover:bg-fuchsia-600 border-black border-opacity-50 rounded-[30.859px] max-md:pl-5 active:border-white duration-300 active:text-white">
-              <li className="list-none" href="/">
-                Fast Moving
-              </li>
-            </button>
-          </div>
+      <div className="w-auto">
+        <div className="mt-[80px] ml-[15px] w-[560px] h-[270px] text-center bg-white rounded-2xl border border-black border-solid">
+          <div className=" gap-5 px-12 py-3  rounded-2xl border-0 border-black border-solid shadow-sm bg-zinc-300 bg-opacity-10  ">
+            <div className="flex gap-5 justify-between text-3xl font-bold text-black">
+              <button className="justify-center px-4 py-4 border border-solid hover:bg-fuchsia-600 border-black border-opacity-50 rounded-[30.859px] active:border-white duration-300 active:text-white">
+                <li className="list-none" href="/">
+                  Slow Moving
+                </li>
+              </button>
+              <button className="justify-center px-6 py-5 bg-white border border-solid hover:bg-fuchsia-600 border-black border-opacity-50 rounded-[30.859px] max-md:pl-5 active:border-white duration-300 active:text-white">
+                <li className="list-none" href="/">
+                  Fast Moving
+                </li>
+              </button>
+            </div>
 
-          <li className="list-none flex-auto my-auto text-3xl text-red-600">
-            <a href="/">View Entire List</a>
-          </li>
+            <li className="list-none flex-auto my-auto text-3xl text-red-600">
+              <a href="/">View Entire List</a>
+            </li>
+          </div>
+          <div className="flex gap-5 px-3 py-5 mx-5 mt-2 text-3xl text-black bg-white border-0 border-black border-solid  ">
+            <div className="flex-auto">Product Name</div>
+            <div className="flex-auto">Company</div>
+            <div>Qty Sold</div>
+            <div>Stock</div>
+          </div>
         </div>
-        <div className="flex gap-5 px-3 py-5 mx-5 mt-2 text-3xl text-black bg-white border-0 border-black border-solid  ">
-          <div className="flex-auto">Product Name</div>
-          <div className="flex-auto">Company</div>
-          <div>Qty Sold</div>
-          <div>Stock</div>
-        </div>
+        <div className=" ml-[15px] mr-[10px] rounded-[14.897px] border-[rgba(0,_0,_0,_1)] border-solid border bg-[#fff] flex flex-grow flex-col text-[#000] font-normal text-center w-[560px] h-[600px] pt-[0] px-[2px] pb-[80px] overflow-x-auto overflow-y-auto"></div>
       </div>
-      <div className=" ml-[15px] mr-[10px] rounded-[14.897px] border-[rgba(0,_0,_0,_1)] border-solid border bg-[#fff] flex flex-grow flex-col text-[#000] font-normal text-center w-[560px] h-[600px] pt-[0] px-[2px] pb-[80px] overflow-x-auto overflow-y-auto"></div>
     </div>
   );
 }
--- src/Frontend/components1/RefilStk.jsx
+++ src/Frontend/components1/RefilStk.jsx
@@ -5,20 +5,22 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 function RefilStk() {
   return (
     <div class="RefilStk">
-      <div class="mt-[10px] ml-[610px]  mr-[10px] rounded-[14.897px] border-[rgba(0,_0,_0,_1)] border-solid border bg-[#fff] flex flex-grow flex-col text-[#000] font-normal text-center w-[600px] h-[80px] pt-[0] px-[2px] pb-[80px]">
-        <div class="rounded-tl-[14.897px] rounded-br-none rounded-tr-[14.897px] rounded-bl-none border-[0px] border-[solid] border-[#000] [box-shadow:0px_5.959px_5.959px_0px_rgba(0,_0,_0,_0.25)] bg-[rgba(217,_217,_217,_0.05)] flex w-[595px] h-[80px] gap-[20px] pl-[24px] pr-[11px] py-[13px]">
-          <div class="flex-grow mx-[0] my-[auto] [font:25px_Calibri,_sans-serif]">
-            Refill Stock Reminders
-          </div>
-          <li class="list-none font-[Calibri,_sans-serif] my-[auto] text-[27px] text-red-600">
-            <a href="/">Entire List Below</a>
-          </li>
-          <div class="text-red-600 text-[50px]">
-            <MdKeyboardArrowDown />
+      <div class="w-auto">
+        <div class="mt-[10px] ml-[610px]  mr-[10px] rounded-[14.897px] border-[rgba(0,_0,_0,_1)] border-solid border bg-[#fff] flex flex-grow flex-col text-[#000] font-normal text-center w-[600px] h-[80px] pt-[0] px-[2px] pb-[80px]">
+          <div class="rounded-tl-[14.897px] rounded-br-none rounded-tr-[14.897px] rounded-bl-none border-[0px] border-[solid] border-[#000] [box-shadow:0px_5.959px_5.959px_0px_rgba(0,_0,_0,_0.25)] bg-[rgba(217,_217,_217,_0.05)] flex w-[595px] h-[80px] gap-[20px] pl-[24px] pr-[11px] py-[13px]">
+            <div class="flex-grow mx-[0] my-[auto] [font:25px_Calibri,_sans-serif]">
+              Refill Stock Reminders
+            </div>
+            <li class="list-none font-[Calibri,_sans-serif] my-[auto] text-[27px] text-red-600">
+              <a href="/">Entire List Below</a>
+            </li>
+            <div class="text-red-600 text-[50px]">
+              <MdKeyboardArrowDown />
+            </div>
           </div>
         </div>
+        <div className=" ml-[10px] mr-[500px] rounded-[14.897px] border-[rgba(0,_0,_0,_1)] border-solid border bg-[#fff] flex flex-grow flex-col text-[#000] font-normal text-center w-[600px] h-[200px] pt-[0] px-[2px] pb-[80px] overflow-x-auto overflow-y-auto"></div>
       </div>
-      <div className=" ml-[10px] mr-[500px] rounded-[14.897px] border-[rgba(0,_0,_0,_1)] border-solid border bg-[#fff] flex flex-grow flex-col text-[#000] font-normal text-center w-[600px] h-[200px] pt-[0] px-[2px] pb-[80px] overflow-x-auto overflow-y-auto"></div>
     </div>
   );
 }
