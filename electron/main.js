b4637caa96d02453007abc808437bafb7c5f515b
Tuhin Das <tuhin@Tuhins-MacBook-Air.local>
2024-07-10 05:00:48 +0530

    deply 3

--- electron/main.js
+++ electron/main.js
@@ -1,37 +1,33 @@
-const { app, BrowserWindow } = require('electron/main')
-const path = require('node:path')
+const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 1024,
-    height: 768,
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: false,
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, './index.html'),
-      protocol: 'file:',
-      slashes: true
-    })
-  );
+  win.loadURL(`http://localhost:5173`);
 
-app.whenReady().then(() => {
-  createWindow()
+  win.on('closed', () => {
+    win = null;
+  });
+}
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})}
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
